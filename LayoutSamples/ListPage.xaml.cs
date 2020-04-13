c6b458e2ae3ab8d8e784d86f9c7a8a4b969b1dfe
Eric Liu <eliu@my.wctc.edu>
2020-04-13 11:54:53 -0500

    Add project files.

---
+++ LayoutSamples/ListPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Hw6
+{
+	public partial class ListPage : ContentPage
+	{
+		public ListPage ()
+		{
+			InitializeComponent ();
+			List<Page> listOfPages = new List<Page> () {
+				new AbsoluteLayout (),
+				new RelativeLayout (),
+				new StackLayout ()
+			};
+
+			listPages.ItemsSource = listOfPages;
+			listPages.ItemSelected += ListPages_ItemSelected;
+		}
+
+		void ListPages_ItemSelected (object sender, SelectedItemChangedEventArgs e)
+		{
+			if (e.SelectedItem != null) {
+				this.Navigation.PushAsync ((Page)e.SelectedItem);
+			}
+			listPages.SelectedItem = null;
+		}
+	}
+}
